c74ae5c4a97c905c46b6abd4196fbee942f8187a
Austin Willis <willisaj@rose-hulman.edu>
2015-01-24 13:29:46 -0500

    Adding simple non-functional view to MainActivity

--- MovieMatch/src/edu/rosehulman/moviematch/MainActivity.java
+++ MovieMatch/src/edu/rosehulman/moviematch/MainActivity.java
@@ -2,8 +2,6 @@ package edu.rosehulman.moviematch;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
