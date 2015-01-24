bd86adb3ac3c428c37927569fdece4b161819c9c
Andrew Davidson <davidsac@rose-hulman.edu>
2015-01-24 18:47:57 -0500

    Fixed havoc caused by last commit

--- MovieMatch/src/edu/rosehulman/moviematch/MovieListActivity.java
+++ MovieMatch/src/edu/rosehulman/moviematch/MovieListActivity.java
@@ -30,25 +30,6 @@ public class MovieListActivity extends Activity {
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.recommendation, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	@Override
 	protected void onSaveInstanceState(Bundle outState) {
 		outState.putParcelableArrayList("list", adapter.getArrayList());
 	}
