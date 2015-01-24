20a42718587fd52d5a98b91f0042e5f2da007496
Andrew Davidson <davidsac@rose-hulman.edu>
2015-01-24 18:44:48 -0500

    Made revisions to activity_main.xml and added spinners to the activity. Madea movie Profile page as well.

--- MovieMatch/src/edu/rosehulman/moviematch/MainActivity.java
+++ MovieMatch/src/edu/rosehulman/moviematch/MainActivity.java
@@ -1,8 +1,9 @@
-
 package edu.rosehulman.moviematch;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends Activity {
 
@@ -10,7 +11,30 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		Spinner genreSpinner = (Spinner) findViewById(R.id.genre_spinner);
+		// Create an ArrayAdapter using the string array and a default spinner
+		// layout
+		ArrayAdapter<CharSequence> genreAdapter = ArrayAdapter
+				.createFromResource(this, R.array.genres,
+						android.R.layout.simple_spinner_item);
+		// Specify the layout to use when the list of choices appears
+		genreAdapter
+				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		// Apply the adapter to the spinner
+		genreSpinner.setAdapter(genreAdapter);
+
+		Spinner distributionSpinner = (Spinner) findViewById(R.id.genre_spinner);
+		// Create an ArrayAdapter using the string array and a default spinner
+		// layout
+		ArrayAdapter<CharSequence> distAdapter = ArrayAdapter
+				.createFromResource(this, R.array.distributions,
+						android.R.layout.simple_spinner_item);
+		// Specify the layout to use when the list of choices appears
+		distAdapter
+				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		// Apply the adapter to the spinner
+		distributionSpinner.setAdapter(distAdapter);
+
 	}
 }
-
-
