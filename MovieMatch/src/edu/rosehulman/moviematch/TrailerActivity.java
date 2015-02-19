ffdca3c8acbb8fb56ea9d5bd2fad9317f0bf9959
Austin Willis <willisaj@rose-hulman.edu>
2015-02-18 21:47:07 -0500

    Revert "Revert "Adding Google and Amazon Support""
    This reverts commit 9e7b5d132708f30283614e220ad8221e8bce175d.

--- MovieMatch/src/edu/rosehulman/moviematch/TrailerActivity.java
+++ MovieMatch/src/edu/rosehulman/moviematch/TrailerActivity.java
@@ -1,49 +1,68 @@
 package edu.rosehulman.moviematch;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.WindowManager;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings.PluginState;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class TrailerActivity extends Activity {
-	
+
 	public static final String KEY_TRAILER_URL = "KEY_TRAILER_URL";
-	
+
 	private String mTrailerUrl;
-	
+
 	private WebView mWebView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_trailer);
-		
+
 		mTrailerUrl = getIntent().getStringExtra(KEY_TRAILER_URL);
-		
+
 		mWebView = (WebView) findViewById(R.id.webview);
 		mWebView.getSettings().setJavaScriptEnabled(true);
-        mWebView.getSettings().setPluginState(PluginState.ON);
-        mWebView.getSettings().setAllowFileAccess(true);
-       
-        mWebView.loadUrl(mTrailerUrl);
-        mWebView.setWebChromeClient(new WebChromeClient() {
-        });
-             
-        mWebView.setWebViewClient(new WebViewClient() {
-                 public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                     return false;
-                }
-                public void onPageFinished(WebView view, String url) {
-                  //do nothing
-                } 
-          });
+		mWebView.getSettings().setPluginState(PluginState.ON);
+		mWebView.getSettings().setAllowFileAccess(true);
+		
+		mWebView.setPadding(0, 0, 0, 0);
+		mWebView.setInitialScale(getScale());
+
+		mWebView.loadUrl(mTrailerUrl);
+		mWebView.setWebChromeClient(new WebChromeClient() {
+		});
+
+		mWebView.setWebViewClient(new WebViewClient() {
+			public boolean shouldOverrideUrlLoading(WebView view, String url) {
+				return false;
+			}
+
+			public void onPageFinished(WebView view, String url) {
+				mWebView.loadUrl("javascript:(function(){"
+						+ "l=document.getElementById('playbutton');"
+						+ "e=document.createEvent('HTMLEvents');"
+						+ "e.initEvent('click',true,true);"
+						+ "l.dispatchEvent(e);" + "})()");
+			}
+		});
 	}
-	
+
 	@Override
 	protected void onDestroy() {
 		mWebView.loadUrl("");
 		super.onDestroy();
+	}
+	
+	private int getScale(){
+	    Display display = ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay(); 
+	    int width = display.getWidth(); 
+	    Double val = new Double(width)/new Double(640);
+	    val = val * 100d;
+	    return val.intValue();
 	}
 }
